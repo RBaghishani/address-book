4483abb5955ac585bbbc85f3c1ea55a073c348b0
Reihaneh <baghishani.r@gmail.com>
2023-06-13 10:43:30 +0200

    fix validation

--- src/main/java/com/unicredit/addressbook/dto/ContactDTO.java
+++ src/main/java/com/unicredit/addressbook/dto/ContactDTO.java
@@ -1,5 +1,6 @@
 package com.unicredit.addressbook.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
@@ -12,10 +13,13 @@ import lombok.NoArgsConstructor;
 public class ContactDTO {
 
     private Long id;
+    @NotBlank
     @Size(min = 2, max = 30)
     private String name;
+    @NotBlank
     @Size(min = 2, max = 30)
     private String surname;
+    @NotBlank
     @Pattern(regexp = "^(\\+\\d{1,2}\\s?)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$")
     private String phoneNumber;
 
